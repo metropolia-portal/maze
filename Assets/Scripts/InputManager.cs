05557449062bf95f725b10a380938141018e2566
FRTT <rocket.coverall@gmail.com>
2013-06-06 15:00:57 +0300

    game states

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class InputManager : MonoBehaviour {
 	
 	Vector3 acceleration;
+	bool isButtonDown = false;
 	bool isEscapeButtonDown = false;
+	bool isApplicationPaused = false;
 	
 
 	
@@ -17,20 +19,37 @@ public class InputManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			isEscapeButtonDown = true;
+		} else {
+			isEscapeButtonDown = false;
+		}
+		
 #if UNITY_ANDROID
 		acceleration = Input.acceleration*sensitivity;
+		isButtonDown = Input.touchCount == 1;
+		
 #else
 		acceleration = new Vector3 (2*Input.mousePosition.x/Screen.width - 1, 2*Input.mousePosition.y/Screen.height - 1, 0)*sensitivity;
+		isButtonDown = Input.GetMouseButtonDown(0);
 #endif
-		
-		if (Input.GetKeyDown(KeyCode.Escape)) {
-			Application.LoadLevel("MazeLevel");
-			Time.timeScale = 1;
-		}
+	}
+	
+	void  OnApplicationPause(bool pauseStatus) {
+		isApplicationPaused = pauseStatus;
+	}
+	
+	public bool IsApplicationPaused() {
+		return isApplicationPaused;
 	}
 	
 	public Vector3 GetAcceleration() {
 		return acceleration;
+	}
+	
+	public bool IsButtonDown() {
+		return isButtonDown;
 	}
 	
 	public bool IsEscapeButtonDown() {
